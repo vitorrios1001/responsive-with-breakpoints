f284d69efdf79561b47f080f101b566539023925
Vitor Rios <vitor.neto@br.experian.com>
2020-05-09 12:06:14 -0300

    Using conext created for demonstration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { useTheme } from './contexts/themeContext';
+
+import './styles.scss'
 
 function App() {
+  const { viewPort } = useTheme()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="content">
+        <p>Viewport: {viewPort.text.toUpperCase()}</p>
+        <span>Resize your screen width to test</span>
+      </div>    
     </div>
   );
 }
